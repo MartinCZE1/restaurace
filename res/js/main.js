05f492c5a708614a75e7c2c7807835f38df77bf8
MartinHercik <martinhercik16@gmail.com>
2023-02-13 10:23:26 +0100

    Init

---
+++ res/js/main.js
@@ -0,0 +1,7 @@
+const startButton = document.getElementById("startButton");
+
+startButton.onclick = () => {
+  start.style.display = "none";
+  startButton.style.display = "none"
+  restaurant.style.display = "block";
+};
