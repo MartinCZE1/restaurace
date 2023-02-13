e5761c6406dabaf42010b8adb16da5aea3e98833
Martin <martinhercik16@gmail.com>
2023-02-13 15:35:17 +0100

    Init

--- res/js/main.js
+++ res/js/main.js
@@ -4,4 +4,7 @@ startButton.onclick = () => {
   start.style.display = "none";
   startButton.style.display = "none"
   restaurant.style.display = "block";
+  ingredients.style.display = "inline-block";
+  table.style.display = "inline-block";
+  order.style.display = "inline-block";
 };
