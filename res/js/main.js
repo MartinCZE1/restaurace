69de3d139a980024289a66e5332151f758068f99
Martin Hercík <martinhercik16@gmail.com>
2023-02-20 10:35:00 +0100

    Init

--- res/js/main.js
+++ res/js/main.js
@@ -1,15 +1,38 @@
 const startButton = document.getElementById("startButton");
+const story = document.getElementById("story");
 const orderButton = document.getElementById("orderButton");
 
 let orderNumber = 0;
 
 startButton.onclick = () => {
+  //Přidat zobrazování textu json
   start.style.display = "none";
   startButton.style.display = "none";
+  //Toto přepsat do jiné fce
   restaurant.style.display = "block";
   ingredients.style.display = "inline-block";
   table.style.display = "inline-block";
   order.style.display = "inline-block";
+};
+
+window.onload = async () => {
+  try {
+    const file = await fetch("./res/data/storyline.json");
+    const data = await file.json();
+    console.log(data);
+    data.story.forEach((line) => {
+      story.innerHTML += `
+      ${line.one}
+      <p>${line.two}</p>
+      <p>${line.three}</p>
+      <p>${line.four}</p>
+      <p>${line.five}</p>
+      <p>${line.six}</p>
+      `;
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 //Zde se vloží sestavené menu místo rand 1, 2, 3, 4, 5
@@ -26,11 +49,10 @@ function orderList() {
   } else if (orderNumber == 5) {
     console.log("rand 5");
   } else {
-    console.log("nasjkdha")
+    console.log("nasjkdha");
   }
 }
 
 orderButton.onclick = () => {
   orderList();
 };
-
